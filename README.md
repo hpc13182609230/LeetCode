2a64155399fda317c99a7b5e7e27527710fdc055
hpc13182609230 <1224579114@qq.com>
2021-03-11 19:40:40 +0800

    twoSum
    两数之和

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# LeetCode
+# leetcode
 算法
---
+++ src/main/java/Common/PrintHelper.java
@@ -0,0 +1,12 @@
+package Common;
+
+import java.util.ArrayList;
+
+public class PrintHelper {
+    public static void printArray(int[] array)
+    {
+        for(int i=0;i<array.length;i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
