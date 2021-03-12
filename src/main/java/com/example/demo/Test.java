21383c85b0839f04f7ae69f6bb7428b4adfc0c2e
hpc13182609230 <1224579114@qq.com>
2021-03-12 18:31:50 +0800

    Test demo

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import Common.PrintHelper;
+
+
+/*
+*题目解释
+ */
+
+
+public class Test {
+
+    @org.junit.Test
+    public void test() {
+        //mock test data
+        //
+        execute();
+        PrintHelper.printArray(null);
+    }
+
+    public void execute() {
+        /*
+         /check something
+         */
+
+        //do main
+
+    }
+
+}
